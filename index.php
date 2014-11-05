7dc3f4efa39d0098faf72980d4911fdc1eba5b1a
Sean Burke <hawaiianchimp@gmail.com>
2014-11-05 09:19:47 -0800

    moving php file

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<a href='/myeplay/game.html'>Game</a>";
+echo "<a href='/myeplay/media.html'>Media</a>";
+
+?>
--- myeplay/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo "<a href='game.html'>Game</a>";
-echo "<a href='media.html'>Media</a>";
-
-?>
