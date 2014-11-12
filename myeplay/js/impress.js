08884f9fdaf095055d6ea36a41b1d12ecf2fab61
Sean Burke <hawaiianchimp@gmail.com>
2014-11-12 00:05:26 -0800

    adding progress bar navigation, improved UI mathematics,

--- myeplay/js/impress.js
+++ myeplay/js/impress.js
@@ -330,7 +330,7 @@
         
         // `init` API function that initializes (and runs) the presentation.
         var init = function () {
-            if (initialized) { return; }
+            //if (initialized) { return; }
             
             // First we set up the viewport for mobile devices.
             // For some reason iPad goes nuts when it is not done properly.
@@ -760,6 +760,60 @@
                 event.preventDefault();
             }
         }, false);
+
+        var duration = 2000;
+        var timeout = null;
+        var circle = new ProgressBar.Circle('#timer', {
+                color: '#FCB03C',
+                strokeWidth: 2,
+                duration: duration,
+            });
+
+        // delegated handler for clicking on step elements
+        document.addEventListener("mouseover", function ( event ) {
+            var target = event.target;
+            // find closest step element that is not active
+            //console.log(target.classList);
+            if( (target.classList.contains("step") && !target.classList.contains("active"))) {
+                
+                circle.duration = duration;
+                circle.animate(1, function(){
+                    circle.set(0);
+                });
+
+                timeout = setTimeout(function(){
+                    if ( api.goto(target) ) {
+                        event.preventDefault();
+                    }
+                }, duration);
+            }
+            
+        }, false);
+
+        // delegated handler for clicking on step elements
+        document.addEventListener("mouseout", function ( event ) {
+            var target = event.target;
+            // find closest step element that is not active
+            if( (target.classList.contains("step") && !target.classList.contains("active"))) {
+                
+                circle.set(0);
+                clearTimeout(timeout);
+                        event.preventDefault();
+            }
+        }, false);
+
+        // delegated handler for clicking on step elements
+        document.addEventListener("mouseleave", function ( event ) {
+            var target = event.target;
+            // find closest step element that is not active
+            while ( !(target.classList.contains("step") && !target.classList.contains("active")) &&
+                    (target !== document.documentElement) ) {
+                target = target.parentNode;
+            }
+            circle.set(0);
+            clearTimeout(timeout);
+                    event.preventDefault();
+        }, false);
         
         // touch handler to detect taps on the left and right side of the screen
         // based on awesome work of @hakimel: https://github.com/hakimel/reveal.js
@@ -790,6 +844,19 @@
     }, false);
         
 })(document, window);
+
+
+var timer = document.createElement("div");
+timer.id = "timer";
+document.body.insertBefore(timer, document.getElementById("impress"));
+
+onmousemove = function(event){
+    var timer = $("#timer");
+    timer.css("position", "absolute");
+    timer.css("top", (event.y -50) + "px");
+    timer.css("left", (event.x- 50) + "px");
+}
+
 
 // THAT'S ALL FOLKS!
 //
